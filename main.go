118875ab8893baec036fc4e9e51c54c95ae81918
heeyeah <heeyeah@shinhan.com>
2021-11-11 14:12:29 +0900

    using custom package : heepack

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/heeyeah/heepack"
 )
 
 func main() {
 	fmt.Println("sample-go, heeye")
+	val := heepack.Reverse("November")
+	fmt.Println("this is November reverse word : " + val)
 }
